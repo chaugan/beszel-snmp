6a11f6ee83d81dc10ff5b1727cc4f500c9df9dcd
chaugan <chaugan@github.com>
2025-09-17 22:05:46 +0200

    Use area charts for temperature and humidity in SNMP monitors
    - Modified TemperatureChart to use AreaChart for SNMP agents instead of LineChart
    - Modified HumidityChart to use AreaChart for SNMP agents instead of LineChart
    - Added isSnmpAgent prop to both chart components
    - Updated system.tsx to pass isSnmpAgent prop to chart components
    - SNMP agents now display temperature and humidity data as area charts for better visualization

--- internal/site/src/components/routes/system.tsx
+++ internal/site/src/components/routes/system.tsx
@@ -705,14 +705,14 @@ export default memo(function SystemDetail({ name }: { name: string }) {
 							description={t`Temperatures of system sensors`}
 							cornerEl={<FilterBar store={$temperatureFilter} />}
 						>
-							<TemperatureChart chartData={chartData} />
+							<TemperatureChart chartData={chartData} isSnmpAgent={isSnmpAgent} />
 						</ChartCard>
 					)}
 
 					{/* Humidity chart */}
 					{systemStats.at(-1)?.stats.h && (
 						<ChartCard empty={dataEmpty} grid={grid} title={t`Humidity`} description={t`Relative humidity of sensors`}>
-							<HumidityChart chartData={chartData} />
+							<HumidityChart chartData={chartData} isSnmpAgent={isSnmpAgent} />
 						</ChartCard>
 					)}
 
